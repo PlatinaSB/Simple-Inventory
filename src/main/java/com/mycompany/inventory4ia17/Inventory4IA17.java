eaf09005753dc52bb5702ac92eeba732d40efeaf
PlatinaSB <59110147+PlatinaSB@users.noreply.github.com>
2025-12-05 16:21:45 +0700

    add Main and app resources

--- src/main/java/com/mycompany/inventory4ia17/Inventory4IA17.java
+++ src/main/java/com/mycompany/inventory4ia17/Inventory4IA17.java
@@ -1,16 +1,39 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.inventory4ia17;
+
+import com.mycompany.inventory4ia17.Controller.ControllerInv;
+import com.mycompany.inventory4ia17.Service.ServiceInv;
+import com.mycompany.inventory4ia17.View.View;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 /**
  *
  * @author Platina Setia Budi
  */
-public class Inventory4IA17 {
+@SpringBootApplication
+public class Inventory4IA17 implements ApplicationRunner {
+
+    @Autowired
+    private ServiceInv ServiceInv;
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.setProperty("java.awt.headless", "false");
+
+        ApplicationContext context = SpringApplication.run(Inventory4IA17.class, args);
+
+        ControllerInv controller = context.getBean(ControllerInv.class);
+        View invView = new View(controller);
+        invView.setVisible(true);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
     }
 }
