ba2549b3a14ffbd02de0f4c1e4a62649482e8fd6
PlatinaSB <59110147+PlatinaSB@users.noreply.github.com>
2025-12-05 11:22:47 +0700

    first Commit

---
+++ src/main/java/com/mycompany/inventory4ia17/Inventory4IA17.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inventory4ia17;
+
+/**
+ *
+ * @author Platina Setia Budi
+ */
+public class Inventory4IA17 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
